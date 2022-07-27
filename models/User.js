3af70e8f1f638b51599797e0241f03a10daaed31
DWKanas <dkanas0122@gmail.com>
2022-07-27 10:26:53 -0700

    updated models

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thoughts');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please put a vaild email address",],
+      },
+      thoughts: [thoughtSchema],
+      friends: [userSchema],
+    },
+    {
+        toJSON: {
+           virtuals: true,
+        },
+    }
+);
+
+userSchema.virtual("friends").get(function (){
+ return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.export = User;
